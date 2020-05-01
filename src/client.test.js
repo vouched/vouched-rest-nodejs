2d19fcb192d963f1fd876b54bb6ead8175ba5fcd
caojoba <57786930+caojoba@users.noreply.github.com>
2020-05-01 16:24:51 -0700

    adding sunglasses test

--- src/client.test.js
+++ src/client.test.js
@@ -87,10 +87,10 @@ describe('jobs', () => {
     expect(items.length).toBeGreaterThan(-1);
   });
   test(
-    'job submit',
+    'job submit sunglasses',
     async () => {
       const userPhoto = await imageToBase64(
-        path.dirname(__filename) + '/../data/test-face.png'
+        path.dirname(__filename) + '/../data/test-sunglasses.jpg'
       );
       const idPhoto = await imageToBase64(
         path.dirname(__filename) + '/../data/test-id.png'
@@ -112,6 +112,47 @@ describe('jobs', () => {
         }
       };
       const job = await fetchApi('/jobs', { body, method: 'POST' });
+      expect(job.result.confidences.selfieSunglasses).toBeGreaterThan(0.97);
+      expect(job.surveyPoll).toBe(null);
+      expect(job.surveyMessage).toBe(null);
+      expect(job.surveyAt).toBe(null);
+      expect(job.result.confidences.faceMatch).toBeLessThan(0.85);
+      expect(job.status).toBe('completed');
+    },
+    20 * 1000
+  );
+  test(
+    'job submit',
+    async () => {
+      const userPhoto = await imageToBase64(
+        path.dirname(__filename) + '/../data/test-face.png'
+      );
+      const idPhoto = await imageToBase64(
+        path.dirname(__filename) + '/../data/test-id.png'
+      );
+      const body = {
+        type: 'id-verification',
+        properties: [
+          {
+            name: 'internal_id',
+            value: 'sdjklfd'
+          }
+        ],
+        params: {
+          firstName: 'John',
+          lastName: 'Bao',
+          dob: '06/22/1970',
+          userPhoto,
+          idPhoto
+        }
+      };
+      const job = await fetchApi('/jobs', { body, method: 'POST' });
+      // console.log(JSON.stringify({ job }, null, 2));
+
+      expect(job.result.confidences.selfieSunglasses).toBe(0);
+      expect(job.result.confidences.id).toBeLessThan(0.85);
+      expect(job.result.confidences.birthDateMatch).toBeGreaterThan(0.9);
+      expect(job.result.confidences.faceMatch).toBeGreaterThan(0.9);
       expect(job.status).toBe('completed');
     },
     20 * 1000
